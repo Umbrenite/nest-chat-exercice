0d7b5b200ba2de0ad1adb2ed454342014c4d6a45
Umbrenite <ablandin2@myges.fr>
2025-05-16 13:27:26 +0200

    make automation via init.sh

--- init.sh
+++ init.sh
@@ -5,12 +5,24 @@ cd "./backend" || { echo "Répertoire backend introuvable"; exit 1; }
 echo "################################################"
 echo "########### INSTALL NPM BACKEND ################"
 echo "################################################"
-npm install
+sudo npm install
 
 cd "../frontend" || { echo "Répertoire frontend introuvable"; exit 1; }
 echo "################################################"
 echo "########## INSTALL NPM FRONTEND ################"
 echo "################################################"
-npm install
+sudo npm install
 
 cd "$ROOT_DIR"
+
+echo "################################################"
+echo "############ LAUNCH CONTAINERS #################"
+echo "################################################"
+docker compose up -d
+
+echo "################################################"
+echo "############# ADD GROUPS TO DB #################"
+echo "################################################"
+
+docker cp ./script.sql postgres-db:/tmp/script.sql
+docker exec postgres-db psql -U postgres -d mydb -f /tmp/script.sql
