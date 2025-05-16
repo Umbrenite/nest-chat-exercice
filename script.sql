27450071d3cea8e0336899f1c64662f85349a3b0
Umbrenite <ablandin2@myges.fr>
2025-05-16 13:54:14 +0200

    correct script image link

--- script.sql
+++ script.sql
@@ -7,4 +7,4 @@ INSERT INTO "group" ("id", "title", "description", "icon_url") VALUES
   (3, 'Marketing Digital', 'Brainstormings, campagnes de communication et suivi des performances publicitaires.', 'https://images.pexels.com/photos/3184292/pexels-photo-3184292.jpeg'), 
   (4, 'Support Client', 'Canal d’échange pour gérer les retours clients, tickets de support et FAQ.', 'https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg'), 
   (5, 'QA & Tests', 'Discussions sur les plans de test, les bugs et la validation des livrables.', 'https://images.pexels.com/photos/3861955/pexels-photo-3861955.jpeg'), 
-  (6, 'Admins & Sécurité', 'Espace privé pour les administrateurs du système et les responsables sécurité.', 'https://images.pexels.com/photos/5380645/pexels-photo-5380645.jpeg');
+  (6, 'Admins & Sécurité', 'Espace privé pour les administrateurs du système et les responsables sécurité.', 'https://images.pexels.com/photos/5380644/pexels-photo-5380644.jpeg');
